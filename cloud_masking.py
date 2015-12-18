6022e9828d2ae869238dc32f3f10056071254f33
Xavier Corredor Llano <xcorredorl@ideam.gov.co>
2015-12-18 14:42:05 -0500

    Fix delete item in menu "SMBYC" when unload plugin

--- cloud_masking.py
+++ cloud_masking.py
@@ -20,8 +20,8 @@
  *                                                                         *
  ***************************************************************************/
 """
-from PyQt4.QtCore import QSettings, QTranslator, qVersion, QCoreApplication
-from PyQt4.QtGui import QAction, QIcon
+from PyQt4.QtCore import QSettings, QTranslator, qVersion, QCoreApplication, QObject, SIGNAL
+from PyQt4.QtGui import QAction, QIcon, QMenu
 # Initialize Qt resources from file resources.py
 import resources
 # Import the code for the dialog
@@ -63,10 +63,19 @@ class CloudMasking:
 
         # Declare instance attributes
         self.actions = []
-        self.menu = self.tr(u'&Cloud masking')
-        # TODO: We are going to let the user set this up in a future iteration
-        self.toolbar = self.iface.addToolBar(u'CloudMasking')
-        self.toolbar.setObjectName(u'CloudMasking')
+        
+        # menu
+        self.menu = None
+        # Check if the menu exists and get it
+        for menu_item in self.iface.mainWindow().menuBar().children(): 
+            if isinstance(menu_item, QMenu) and menu_item.title() == u"SMBYC":
+                self.menu = child
+        # If the menu does not exist, create it!
+        if not self.menu:
+            self.menu = QMenu(self.iface.mainWindow().menuBar())
+            self.menu.setObjectName(u'Plugins for the project SMBYC')
+            self.menu.setTitle(u"SMBYC")
+            
 
     # noinspection PyMethodMayBeStatic
     def tr(self, message):
@@ -84,99 +93,26 @@ class CloudMasking:
         return QCoreApplication.translate('CloudMasking', message)
 
 
-    def add_action(
-        self,
-        icon_path,
-        text,
-        callback,
-        enabled_flag=True,
-        add_to_menu=True,
-        add_to_toolbar=True,
-        status_tip=None,
-        whats_this=None,
-        parent=None):
-        """Add a toolbar icon to the toolbar.
-
-        :param icon_path: Path to the icon for this action. Can be a resource
-            path (e.g. ':/plugins/foo/bar.png') or a normal file system path.
-        :type icon_path: str
-
-        :param text: Text that should be shown in menu items for this action.
-        :type text: str
-
-        :param callback: Function to be called when the action is triggered.
-        :type callback: function
-
-        :param enabled_flag: A flag indicating if the action should be enabled
-            by default. Defaults to True.
-        :type enabled_flag: bool
-
-        :param add_to_menu: Flag indicating whether the action should also
-            be added to the menu. Defaults to True.
-        :type add_to_menu: bool
-
-        :param add_to_toolbar: Flag indicating whether the action should also
-            be added to the toolbar. Defaults to True.
-        :type add_to_toolbar: bool
-
-        :param status_tip: Optional text to show in a popup when mouse pointer
-            hovers over the action.
-        :type status_tip: str
-
-        :param parent: Parent widget for the new action. Defaults None.
-        :type parent: QWidget
-
-        :param whats_this: Optional text to show in the status bar when the
-            mouse pointer hovers over the action.
-
-        :returns: The action that was created. Note that the action is also
-            added to self.actions list.
-        :rtype: QAction
-        """
-
-        icon = QIcon(icon_path)
-        action = QAction(icon, text, parent)
-        action.triggered.connect(callback)
-        action.setEnabled(enabled_flag)
-
-        if status_tip is not None:
-            action.setStatusTip(status_tip)
-
-        if whats_this is not None:
-            action.setWhatsThis(whats_this)
-
-        if add_to_toolbar:
-            self.toolbar.addAction(action)
-
-        if add_to_menu:
-            self.iface.addPluginToMenu(
-                self.menu,
-                action)
-
-        self.actions.append(action)
-
-        return action
-
     def initGui(self):
         """Create the menu entries and toolbar icons inside the QGIS GUI."""
 
         icon_path = ':/plugins/CloudMasking/icon.png'
-        self.add_action(
-            icon_path,
-            text=self.tr(u'Cloud masking'),
-            callback=self.run,
-            parent=self.iface.mainWindow())
+        self.action = QAction(QIcon(icon_path), self.tr(u'&Cloud masking'), self.iface.mainWindow())
+        self.action.setObjectName(u'CloudMasking')
+        self.action.setWhatsThis(self.tr(u'Cloud masking ...'))
+        self.action.setStatusTip(self.tr(u'This is status tip'))
+        QObject.connect(self.action, SIGNAL("triggered()"), self.run)
+        self.menu.addAction(self.action)
+        
+        self.menuBar = self.iface.mainWindow().menuBar()
+        self.menuBar.insertMenu(self.iface.firstRightStandardMenu().menuAction(), self.menu)
 
 
     def unload(self):
-        """Removes the plugin menu item and icon from QGIS GUI."""
-        for action in self.actions:
-            self.iface.removePluginMenu(
-                self.tr(u'&Cloud masking'),
-                action)
-            self.iface.removeToolBarIcon(action)
-        # remove the toolbar
-        del self.toolbar
+        for menu_item in self.iface.mainWindow().menuBar().children(): 
+            if isinstance(menu_item, QMenu) and menu_item.title() == u"SMBYC":
+                menu_item.removeAction(self.action)
+                # TODO: remove menu_item "SMBYC" if this is empty (actions)
 
 
     def run(self):
