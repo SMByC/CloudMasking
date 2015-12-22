0f6e1d7380cd3185058ca68dfdb764e0416e40f1
Xavier Corredor Llano <xcorredorl@ideam.gov.co>
2015-12-22 00:08:56 -0500

    Synchronize the slider and spinbox of the cloud probability

--- cloud_masking_dialog.py
+++ cloud_masking_dialog.py
@@ -23,13 +23,17 @@
 
 import os
 
-from PyQt4 import QtGui, uic
+from PyQt4 import QtGui, uic, QtCore
 
 FORM_CLASS, _ = uic.loadUiType(os.path.join(
     os.path.dirname(__file__), 'cloud_masking_dialog_base.ui'))
 
 
 class CloudMaskingDialog(QtGui.QDialog, FORM_CLASS):
+
+    # Fmask parameters
+    cloud_prob = 22.5
+
     def __init__(self, parent=None):
         """Constructor."""
         super(CloudMaskingDialog, self).__init__(parent)
@@ -39,3 +43,22 @@ class CloudMaskingDialog(QtGui.QDialog, FORM_CLASS):
         # http://qt-project.org/doc/qt-4.8/designer-using-a-ui-file.html
         # #widgets-and-dialogs-with-auto-connect
         self.setupUi(self)
+
+        self.setup_gui()
+
+    def setup_gui(self):
+        self.update_cloud_prob_to_slider(self.cloud_prob)
+        self.update_cloud_prob_to_spinbox(self.cloud_prob*10)
+        self.horizontalSlider_CP.valueChanged.connect(self.update_cloud_prob_to_spinbox)
+        self.doubleSpinBox_CP.valueChanged.connect(self.update_cloud_prob_to_slider)
+
+    @QtCore.pyqtSlot(int)
+    def update_cloud_prob_to_slider(self, value):
+        self.cloud_prob = value
+        self.horizontalSlider_CP.setValue(int(value*10))
+
+    @QtCore.pyqtSlot(int)
+    def update_cloud_prob_to_spinbox(self, value):
+        self.cloud_prob = value / 10.0
+        self.doubleSpinBox_CP.setValue(self.cloud_prob)
+
