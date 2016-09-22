4e393e2be83f1806f535c60c1f45804b51c1c916
Xavier Corredor Llano <xcorredorl@ideam.gov.co>
2016-09-22 10:19:19 -0500

    Set the reflective and thermal files names

--- cloud_filters.py
+++ cloud_filters.py
@@ -18,6 +18,7 @@
  *                                                                         *
  ***************************************************************************/
 """
+import os
 
 
 class CloudMaskingResult(object):
@@ -27,6 +28,23 @@ class CloudMaskingResult(object):
     def __init__(self, mtl_path, mtl_file, tmp_dir=None):
         self.mtl_path = mtl_path
         self.mtl_file = mtl_file
+        # dir to input landsat files
+        self.input_dir = os.path.dirname(mtl_path)
         # tmp dir for process
         self.tmp_dir = tmp_dir
 
+        # get_metadata
+        self.landsat_version = int(self.mtl_file['SPACECRAFT_ID'].split('_')[-1])
+        if self.landsat_version == 8:
+            # get the reflective file names bands
+            self.reflective_bands = [
+                os.path.join(self.input_dir, self.mtl_file['FILE_NAME_BAND_'+str(N)])
+                for N in [1,2,3,4,5,6,7,9]]
+            # get the thermal file names bands
+            self.thermal_bands = [
+                os.path.join(self.input_dir, self.mtl_file['FILE_NAME_BAND_' + str(N)])
+                for N in [10,11]]
+
+
+    def do_fmask(self):
+        pass
