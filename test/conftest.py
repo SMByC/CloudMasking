33ff780081befba5545367e15ec371f5f2605026
Xavier Corredor Llano <xcorredorl@ideam.gov.co>
2018-05-18 19:22:18 -0500

    Implemented the pytest fixture for load the project in the pythonpath

---
+++ test/conftest.py
@@ -0,0 +1,13 @@
+import os
+import sys
+import pytest
+
+
+@pytest.fixture(scope="session", autouse=True)
+def python_path_project():
+    # add project dir to pythonpath
+    project_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+
+    if project_dir not in sys.path:
+        sys.path.append(project_dir)
+        print(sys.path)
