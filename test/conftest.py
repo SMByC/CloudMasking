c6d02f90d420361fe5294467d994c1eae22a52ad
Xavier Corredor Llano <xcorredorl@ideam.gov.co>
2018-05-18 23:26:23 -0500

    Finally fix test in pipeline

--- test/conftest.py
+++ test/conftest.py
@@ -6,9 +6,6 @@ import pytest
 @pytest.fixture(scope="session", autouse=True)
 def python_path_project():
     # add project dir to pythonpath
-    project_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
-    print(os.path.realpath(__file__))
-    print("yesss")
-    print(project_dir)
+    project_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
     if project_dir not in sys.path:
         sys.path.append(project_dir)
