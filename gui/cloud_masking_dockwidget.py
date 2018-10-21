75cd554c3458732fac3cf52c050989db3cbd79b3
Xavier Corredor Llano <xcorredorl@ideam.gov.co>
2018-10-20 23:19:17 -0500

    Simplify the about/version plugin button (grafted from 1664e0bb96adb1cd40deb504f227f61e918e89df)

--- gui/cloud_masking_dockwidget.py
+++ gui/cloud_masking_dockwidget.py
@@ -87,7 +87,7 @@ class CloudMaskingDockWidget(QtGui.QDockWidget, FORM_CLASS):
     def setup_gui(self):
         # plugin info #########
         self.about_dialog = AboutDialog()
-        self.QPBtn_PluginInfo.setText(self.tr(u"CloudMasking v{}".format(VERSION)))
+        self.QPBtn_PluginInfo.setText("v{}".format(VERSION))
         self.QPBtn_PluginInfo.clicked.connect(self.about_dialog.show)
         self.QPBtn_PluginDocs.clicked.connect(lambda: webbrowser.open("https://smbyc.bitbucket.io/qgisplugins/cloudmasking"))
 
